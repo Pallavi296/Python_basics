fb7e8a88b17ef3d1db56294695fdd2b648369f50
Pallavi296 <pallavi.m@azguardstech.com>
2025-08-05 17:38:36 +0530

    Updated  FastAPI Todo App project

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,11 @@
-from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel, constr, Field, conint
 
 class TodoBase(BaseModel):
-    title: str
-    description: str
-    completed: bool = False
-
+    title: constr(min_length=6)                                #Pyndate for data validation and data parsing
+    description: constr(max_length=40)
+    completed: Optional[datetime] = datetime.now()
 class TodoCreate(TodoBase):
     pass
 
@@ -12,7 +13,7 @@ class TodoUpdate(TodoBase):
     pass
 
 class TodoOut(TodoBase):
-    id: int
+    id: conint(gt=100)
 
     model_config = {
         "from_attributes": True
